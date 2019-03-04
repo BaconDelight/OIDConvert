97d398433de7ba7f574e8d024ce091c366dd64fc
BaconDelight <info@bacondelight.co.uk>
2019-03-04 15:52:07 +0000

    Update 2019-03-04 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,10 +39,10 @@
             // 
             // ConvertToByteArray
             // 
-            this.ConvertToByteArray.Location = new System.Drawing.Point(71, 298);
-            this.ConvertToByteArray.Margin = new System.Windows.Forms.Padding(1);
+            this.ConvertToByteArray.Location = new System.Drawing.Point(178, 745);
+            this.ConvertToByteArray.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ConvertToByteArray.Name = "ConvertToByteArray";
-            this.ConvertToByteArray.Size = new System.Drawing.Size(180, 40);
+            this.ConvertToByteArray.Size = new System.Drawing.Size(450, 100);
             this.ConvertToByteArray.TabIndex = 0;
             this.ConvertToByteArray.Text = "Convert to Byte Array";
             this.ConvertToByteArray.UseVisualStyleBackColor = true;
@@ -50,10 +50,10 @@
             // 
             // ConvertToDottedDecimal
             // 
-            this.ConvertToDottedDecimal.Location = new System.Drawing.Point(444, 298);
-            this.ConvertToDottedDecimal.Margin = new System.Windows.Forms.Padding(1);
+            this.ConvertToDottedDecimal.Location = new System.Drawing.Point(1110, 745);
+            this.ConvertToDottedDecimal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ConvertToDottedDecimal.Name = "ConvertToDottedDecimal";
-            this.ConvertToDottedDecimal.Size = new System.Drawing.Size(180, 40);
+            this.ConvertToDottedDecimal.Size = new System.Drawing.Size(450, 100);
             this.ConvertToDottedDecimal.TabIndex = 1;
             this.ConvertToDottedDecimal.Text = "Convert to Dotted Decimal";
             this.ConvertToDottedDecimal.UseVisualStyleBackColor = true;
@@ -62,59 +62,59 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Location = new System.Drawing.Point(48, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.Size = new System.Drawing.Size(209, 32);
             this.label1.TabIndex = 4;
             this.label1.Text = "Dotted Decimal";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(379, 18);
-            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Location = new System.Drawing.Point(948, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(146, 32);
             this.label2.TabIndex = 5;
             this.label2.Text = "Byte Array";
             // 
             // textBoxDottedDecimal
             // 
-            this.textBoxDottedDecimal.Location = new System.Drawing.Point(16, 38);
-            this.textBoxDottedDecimal.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxDottedDecimal.Location = new System.Drawing.Point(40, 95);
+            this.textBoxDottedDecimal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxDottedDecimal.Multiline = true;
             this.textBoxDottedDecimal.Name = "textBoxDottedDecimal";
             this.textBoxDottedDecimal.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxDottedDecimal.Size = new System.Drawing.Size(291, 227);
+            this.textBoxDottedDecimal.Size = new System.Drawing.Size(722, 562);
             this.textBoxDottedDecimal.TabIndex = 6;
             // 
             // textBoxByteArray
             // 
-            this.textBoxByteArray.Location = new System.Drawing.Point(372, 38);
-            this.textBoxByteArray.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxByteArray.Location = new System.Drawing.Point(930, 95);
+            this.textBoxByteArray.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxByteArray.Multiline = true;
             this.textBoxByteArray.Name = "textBoxByteArray";
             this.textBoxByteArray.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxByteArray.Size = new System.Drawing.Size(364, 227);
+            this.textBoxByteArray.Size = new System.Drawing.Size(904, 562);
             this.textBoxByteArray.TabIndex = 7;
             // 
             // cbFormat
             // 
             this.cbFormat.AutoSize = true;
-            this.cbFormat.Location = new System.Drawing.Point(89, 272);
-            this.cbFormat.Margin = new System.Windows.Forms.Padding(1);
+            this.cbFormat.Location = new System.Drawing.Point(222, 680);
+            this.cbFormat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbFormat.Name = "cbFormat";
-            this.cbFormat.Size = new System.Drawing.Size(143, 17);
+            this.cbFormat.Size = new System.Drawing.Size(374, 36);
             this.cbFormat.TabIndex = 8;
             this.cbFormat.Text = "C# byte[] initialiser format";
             this.cbFormat.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(240F, 240F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(757, 358);
+            this.ClientSize = new System.Drawing.Size(1892, 895);
             this.Controls.Add(this.cbFormat);
             this.Controls.Add(this.textBoxByteArray);
             this.Controls.Add(this.textBoxDottedDecimal);
@@ -122,7 +122,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ConvertToDottedDecimal);
             this.Controls.Add(this.ConvertToByteArray);
-            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OID Convert";
