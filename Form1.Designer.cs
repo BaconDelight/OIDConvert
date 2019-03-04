adf43e81723ec42731b15b4b440890df052e0f71
BaconDelight <info@bacondelight.co.uk>
2019-03-04 15:14:49 +0000

    Update 2018-03-04

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace OIDConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertToByteArray = new System.Windows.Forms.Button();
+            this.ConvertToDottedDecimal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDottedDecimal = new System.Windows.Forms.TextBox();
+            this.textBoxByteArray = new System.Windows.Forms.TextBox();
+            this.cbFormat = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ConvertToByteArray
+            // 
+            this.ConvertToByteArray.Location = new System.Drawing.Point(71, 298);
+            this.ConvertToByteArray.Margin = new System.Windows.Forms.Padding(1);
+            this.ConvertToByteArray.Name = "ConvertToByteArray";
+            this.ConvertToByteArray.Size = new System.Drawing.Size(180, 40);
+            this.ConvertToByteArray.TabIndex = 0;
+            this.ConvertToByteArray.Text = "Convert to Byte Array";
+            this.ConvertToByteArray.UseVisualStyleBackColor = true;
+            this.ConvertToByteArray.Click += new System.EventHandler(this.ConvertToByteArray_Click);
+            // 
+            // ConvertToDottedDecimal
+            // 
+            this.ConvertToDottedDecimal.Location = new System.Drawing.Point(444, 298);
+            this.ConvertToDottedDecimal.Margin = new System.Windows.Forms.Padding(1);
+            this.ConvertToDottedDecimal.Name = "ConvertToDottedDecimal";
+            this.ConvertToDottedDecimal.Size = new System.Drawing.Size(180, 40);
+            this.ConvertToDottedDecimal.TabIndex = 1;
+            this.ConvertToDottedDecimal.Text = "Convert to Dotted Decimal";
+            this.ConvertToDottedDecimal.UseVisualStyleBackColor = true;
+            this.ConvertToDottedDecimal.Click += new System.EventHandler(this.ConvertToDottedDecimal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dotted Decimal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Byte Array";
+            // 
+            // textBoxDottedDecimal
+            // 
+            this.textBoxDottedDecimal.Location = new System.Drawing.Point(16, 38);
+            this.textBoxDottedDecimal.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxDottedDecimal.Multiline = true;
+            this.textBoxDottedDecimal.Name = "textBoxDottedDecimal";
+            this.textBoxDottedDecimal.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDottedDecimal.Size = new System.Drawing.Size(291, 227);
+            this.textBoxDottedDecimal.TabIndex = 6;
+            // 
+            // textBoxByteArray
+            // 
+            this.textBoxByteArray.Location = new System.Drawing.Point(372, 38);
+            this.textBoxByteArray.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxByteArray.Multiline = true;
+            this.textBoxByteArray.Name = "textBoxByteArray";
+            this.textBoxByteArray.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxByteArray.Size = new System.Drawing.Size(364, 227);
+            this.textBoxByteArray.TabIndex = 7;
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.AutoSize = true;
+            this.cbFormat.Location = new System.Drawing.Point(89, 272);
+            this.cbFormat.Margin = new System.Windows.Forms.Padding(1);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(143, 17);
+            this.cbFormat.TabIndex = 8;
+            this.cbFormat.Text = "C# byte[] initialiser format";
+            this.cbFormat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(757, 358);
+            this.Controls.Add(this.cbFormat);
+            this.Controls.Add(this.textBoxByteArray);
+            this.Controls.Add(this.textBoxDottedDecimal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConvertToDottedDecimal);
+            this.Controls.Add(this.ConvertToByteArray);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OID Convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertToByteArray;
+        private System.Windows.Forms.Button ConvertToDottedDecimal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDottedDecimal;
+        private System.Windows.Forms.TextBox textBoxByteArray;
+        private System.Windows.Forms.CheckBox cbFormat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿
+//
+// This program is async Form wrap around the code from here
+//
+// OID Conversion
+// by Miroslav Stampar
+// https://www.codeproject.com/Articles/16468/OID-Conversation
+//
+
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OIDConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] OidStringToByteArray(string oid)
+        {
+            string[] split = oid.Trim(' ', '.').Split('.');
+            List<int> retVal = new List<int>();
+
+            for (int a = 0, b = 0, i = 0; i < split.Length; i++)
+            {
+                if (i == 0)
+                    a = int.Parse(split[0]);
+                else if (i == 1)
+                    retVal.Add(40 * a + int.Parse(split[1]));
+                else
+                {
+                    b = int.Parse(split[i]);
+
+                    if (b < 128)
+                        retVal.Add(b);
+                    else
+                    {
+                        retVal.Add(128 + (b / 128));
+                        retVal.Add(b % 128);
+                    }
+                }
+            }
+
+            byte[] temp = new byte[retVal.Count];
+
+            for (int i = 0; i < retVal.Count; i++)
+                temp[i] = (byte)retVal[i];
+
+            return temp;
+
+        }
+
+        public string OidByteArrayToString(byte[] oid)
+        {
+            StringBuilder retVal = new StringBuilder();
+
+            for (int i = 0; i < oid.Length; i++)
+            {
+                if (i == 0)
+                {
+                    int b = oid[0] % 40;
+                    int a = (oid[0] - b) / 40;
+                    retVal.AppendFormat("{0}.{1}", a, b);
+                }
+                else
+                {
+                    if (oid[i] < 128)
+                        retVal.AppendFormat(".{0}", oid[i]);
+                    else
+                    {
+                        retVal.AppendFormat(".{0}",
+                           ((oid[i] - 128) * 128) + oid[i + 1]);
+                        i++;
+                    }
+                }
+            }
+
+            return retVal.ToString();
+        }
+
+        private void ConvertToByteArray_Click(object sender, System.EventArgs e)
+        {
+            StringBuilder strBody = new StringBuilder();
+            textBoxByteArray.Text = "";
+
+            foreach (string Line in textBoxDottedDecimal.Text.Replace("\r\n","\n").Split('\n'))
+            {
+              string strTemp = Line.Trim();
+
+                if (strTemp.Length != 0)
+                {
+                    byte[] bytes = OidStringToByteArray(strTemp);
+
+                    if (cbFormat.Checked)
+                    {
+                        bool first = true;
+
+                        foreach (byte abyte in bytes)
+                        {
+                            if (first)
+                            {
+                                first = false;
+                                strBody.Append("{ 0x");
+                            }
+                            else
+                            {
+                                strBody.Append(", 0x");
+                            }
+
+                            strBody.Append(abyte.ToString("X2"));
+                        }
+
+                        strBody.Append(" };");
+
+                    }
+                    else
+                    {
+                        foreach (byte abyte in bytes)
+                        {
+                            strBody.Append(abyte.ToString("X2") + " ");
+                        }
+                    }
+
+                    strBody.Append("\r\n");
+                }
+            }
+
+            textBoxByteArray.Text = strBody.ToString();
+        }
+
+        private void ConvertToDottedDecimal_Click(object sender, System.EventArgs e)
+        {
+            StringBuilder strBody = new StringBuilder();
+            textBoxDottedDecimal.Text = "";
+
+            foreach (string Line in textBoxByteArray.Text.Replace("\r\n", "\n").Split('\n'))
+            {
+                string strTemp = Line.Trim().Replace(",", "").Replace("0x", "").Replace("{", "").Replace("}", "").Replace(";","").Trim();
+
+                while (strTemp.IndexOf("  ") >= 0)
+                    strTemp = strTemp.Replace("  ", " ").Trim();
+
+                if (strTemp.Length != 0)
+                {
+                    int count = strTemp.Split(' ').Length;
+                    byte[] bytes = new byte[count];
+
+                    int index = 0;
+
+                    foreach (string strByte in strTemp.Split(' '))
+                        bytes[index++] = byte.Parse(strByte, System.Globalization.NumberStyles.HexNumber);
+
+                    strBody.Append(OidByteArrayToString(bytes) + "\r\n");
+                }
+            }
+
+            textBoxDottedDecimal.Text = strBody.ToString();
+        }
+    }
+}
